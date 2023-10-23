3fb19041addd019bea6152eb35a84510776cc0b5
chill31 <89910163+chill31@users.noreply.github.com>
2023-10-23 14:50:45 +0530

    added dark mode

--- app.vue
+++ app.vue
@@ -1,81 +1,102 @@
 <script setup>
-  const todos = ref([]);
-  
-  onMounted(() => {
-    const todosFromStorage = localStorage.getItem('chill31-nuxt-todo-data');
-    if (todosFromStorage) {
-      todos.value = JSON.parse(todosFromStorage);
-    }
-  });
+const todos = ref([]);
 
-  const todoFormVisible = ref(false);
-  const formTitle = ref('');
-  const formValue = ref('');
-
-
-  function toggleTODOForm() {
-    todoFormVisible.value = !todoFormVisible.value;
-    formTitle.value = '';
-    formValue.value = '';
+onMounted(() => {
+  const todosFromStorage = localStorage.getItem('chill31-nuxt-todo-data');
+  if (todosFromStorage) {
+    todos.value = JSON.parse(todosFromStorage);
   }
+});
 
-  function addTODO() {
-    localStorage.setItem('chill31-nuxt-todo-data', JSON.stringify([...todos.value, {title: formTitle.value, content: formValue.value, done: false}]));
-    todos.value.push({ title: formTitle.value, content: formValue.value, done: false });
-    toggleTODOForm();
-  }
+const todoFormVisible = ref(false);
+const formTitle = ref('');
+const formValue = ref('');
 
-  function deleteTODO(index) {
-    todos.value.splice(index, 1);
-    localStorage.setItem('chill31-nuxt-todo-data', JSON.stringify(todos.value));
-  }
 
-  function toggleDone(index) {
-    todos.value[index].done = !todos.value[index].done;
-    localStorage.setItem('chill31-nuxt-todo-data', JSON.stringify(todos.value));
-  }
+function toggleTODOForm() {
+  todoFormVisible.value = !todoFormVisible.value;
+  formTitle.value = '';
+  formValue.value = '';
+}
+
+function addTODO() {
+  localStorage.setItem('chill31-nuxt-todo-data', JSON.stringify([...todos.value, { title: formTitle.value, content: formValue.value, done: false }]));
+  todos.value.push({ title: formTitle.value, content: formValue.value, done: false });
+  toggleTODOForm();
+}
+
+function deleteTODO(index) {
+  todos.value.splice(index, 1);
+  localStorage.setItem('chill31-nuxt-todo-data', JSON.stringify(todos.value));
+}
+
+function toggleDone(index) {
+  todos.value[index].done = !todos.value[index].done;
+  localStorage.setItem('chill31-nuxt-todo-data', JSON.stringify(todos.value));
+}
 </script>
 
 <template>
-  <div class="min-h-screen w-screen overflow-x-hidden flex flex-col gap-12 items-center justify-start bg-slate-900 selection:bg-zinc-600/30 py-8">
+  <ThemeButton />
+  <div
+    class="min-h-screen w-screen overflow-x-hidden flex flex-col gap-12 items-center justify-start dark:bg-slate-900 dark:selection:bg-zinc-600/30 selection:bg-zinc-400/50 bg-slate-200 py-8">
     <h1 class="text-6xl text-center">TODO</h1>
-    <p class="text-center">Built with <code class="px-2 py-1 border border-white/25 bg-zinc-500/10 rounded-md">Nuxt.js</code> with the help of <code class="px-2 py-1 border border-white/25 bg-zinc-600/10 rounded-md">@nuxt/ui</code></p>
+    <p class="text-center">Built with <code
+        class="px-2 py-1 border border-white/25 bg-zinc-500/10 rounded-md">Nuxt.js</code> with the help of <code
+        class="px-2 py-1 border border-white/25 bg-zinc-600/10 rounded-md">@nuxt/ui</code></p>
 
-    <UButton class="mt-16 flex items-center justify-center gap-2" icon="i-heroicons-plus" size="xl" color="gray" v-if="!todoFormVisible" v-on:click="toggleTODOForm()">Add TODO</UButton>
+    <UButton class="mt-16 flex items-center justify-center gap-2" icon="i-heroicons-plus" size="xl" color="gray"
+      v-if="!todoFormVisible" v-on:click="toggleTODOForm()">Add TODO</UButton>
 
-    <UButton class="mt-16 flex items-center justify-center gap-2" icon="i-heroicons-x-circle" size="xl" color="red" v-if="todoFormVisible" v-on:click="toggleTODOForm()">Cancel</UButton>
+    <UButton class="mt-16 flex items-center justify-center gap-2" icon="i-heroicons-x-circle" size="xl" color="red"
+      v-if="todoFormVisible" v-on:click="toggleTODOForm()">Cancel</UButton>
     <div class="flex flex-col gap-2 items-start justify-start w-[25rem] max-w-[65vw]" v-if="todoFormVisible">
 
       <label for="form-input">Add TODO</label>
-      <UInput id="form-input" class="w-full" color="gray" variant="outline" size="xl" v-model="formTitle" placeholder="enter todo title..."></UInput>
-      <UTextarea id="form-input" class="w-full" color="gray" variant="outline" size="xl" v-model="formValue" placeholder="enter todo content here..."></UTextarea>
+      <UInput id="form-input" class="w-full" color="gray" variant="outline" size="xl" v-model="formTitle"
+        placeholder="enter todo title..."></UInput>
+      <UTextarea id="form-input" class="w-full" color="gray" variant="outline" size="xl" v-model="formValue"
+        placeholder="enter todo content here..."></UTextarea>
 
-      <UButton icon="i-heroicons-plus" class="mt-4 self-end" size="lg" v-on:click="addTODO()" :disabled="formValue.length === 0 || formTitle.length === 0">Add</UButton>
+      <UButton icon="i-heroicons-plus" class="mt-4 self-end" size="lg" v-on:click="addTODO()"
+        :disabled="formValue.length === 0 || formTitle.length === 0">Add</UButton>
 
     </div>
 
     <div class="flex w-screen flex-wrap items-start justify-center gap-4">
       <div v-for="todo, k in todos">
-        
-        <div class="grid auto-cols-auto auto-rows-auto gap-2 w-[20rem] max-w-[calc(98vw-1rem)] py-4 px-2 bg-white/10 selection:bg-white/10 rounded-lg relative rounded-tl-none mt-[2.65rem] border-t-slate-500 border-t-2">
-          <div class="absolute top-[calc(-2.5rem-2px)] left-0 w-[10rem] max-w-[50%] bg-white/10 rounded-lg rounded-b-none px-4 py-2 text-[1.5rem] flex items-center justify-start gap-2">
+
+        <div
+          class="grid auto-cols-auto auto-rows-auto gap-2 w-[20rem] max-w-[calc(98vw-1rem)] py-4 px-2 bg-zinc-300 dark:bg-white/10 selection:bg-white/10 rounded-lg relative rounded-tl-none mt-[2.65rem] border-t-slate-500 border-t-2">
+          <div
+            class="absolute top-[calc(-2.5rem-2px)] left-0 w-[10rem] max-w-[50%] bg-zinc-300 dark:bg-white/10 rounded-lg rounded-b-none px-4 py-2 text-[1.5rem] flex items-center justify-start gap-2">
             <UIcon v-if="todo.done" name="i-bi-check-lg"></UIcon>
-            <span v-if="todo.done" class="text-sm text-slate-400">(done)</span>
+            <span v-if="todo.done" class="text-sm dark:text-slate-400 text-slate-600">(done)</span>
             <UIcon v-if="!todo.done" name="i-bi-clock"></UIcon>
-            <span v-if="!todo.done" class="text-sm text-slate-400">(pending)</span>
+            <span v-if="!todo.done" class="text-sm dark:text-slate-400 text-slate-600">(pending)</span>
           </div>
-          <h3 class="text-[1.5rem] font-semibold overflow-hidden max-w-[80%]" :class="todo.done ? 'text-slate-400' : ''">{{ todo.title }}</h3>
-          <p class="[line-break:anywhere] h-[15ch] overflow-y-scroll text-sm" :class="todo.done ? 'text-slate-400' : ''">{{ todo.content }}</p>
+          <h3 class="text-[1.5rem] font-semibold overflow-hidden max-w-[80%]"
+            :class="todo.done ? 'dark:text-slate-400 text-slate-600' : ''">{{ todo.title }}</h3>
+          <p class="[line-break:anywhere] h-[15ch] overflow-y-scroll text-sm"
+            :class="todo.done ? 'dark:text-slate-400 text-slate-600' : ''">{{ todo.content }}</p>
 
-          <div class="flex items-center justify-center gap-2 mt-8 ml-auto px-4 py-2 rounded-md bg-white/10">
-            <UButton icon="i-ic-baseline-check" color="transparent" class="!text-gray-400 !p-1 focus:outline-current rounded-md hover:bg-white/10" size="sm" v-if="!todo.done" v-on:click="toggleDone(k)"></UButton>
-            <UButton icon="i-bi-x-lg" color="transparent" class="!text-gray-400 !p-1 focus:outline-current rounded-md hover:bg-white/10" size="sm" v-if="todo.done" v-on:click="toggleDone(k)"></UButton>
-            <UButton icon="i-mdi-pencil" color="transparent" class="!text-primary-400 !p-1 focus:outline-current rounded-md hover:bg-white/10" size="sm"></UButton>
-            <UButton icon="i-heroicons-trash" color="transparent" class="!text-red-500 !p-1 focus:outline-current rounded-md hover:bg-white/10" size="sm" v-on:click="deleteTODO(k)"></UButton>
-          </div>
+          <div
+            class="flex items-center justify-center gap-2 mt-8 ml-auto px-4 py-2 rounded-md dark:bg-white/10 bg-zinc-400/20">
+          <UButton icon="i-ic-baseline-check" color="transparent"
+            class="dark:text-gray-400 text-gray-700 bg-slate-300 dark:bg-inherit !p-1 focus:outline-current rounded-md hover:bg-white/10"
+            size="sm" v-if="!todo.done" v-on:click="toggleDone(k)"></UButton>
+          <UButton icon="i-bi-x-lg" color="transparent"
+            class="dark:text-gray-400 text-gray-700 bg-slate-300 dark:bg-inherit !p-1 focus:outline-current rounded-md hover:bg-white/10"
+            size="sm" v-if="todo.done" v-on:click="toggleDone(k)"></UButton>
+          <UButton icon="i-mdi-pencil" color="transparent"
+            class="dark:text-primary-400 text-primary-500 dark:bg-inherit bg-slate-300 !p-1 focus:outline-current rounded-md hover:bg-white/10"
+            size="sm"></UButton>
+          <UButton icon="i-heroicons-trash" color="transparent"
+            class="dark:text-red-500 text-red-600 dark:bg-inherit bg-slate-300 !p-1 focus:outline-current rounded-md hover:bg-white/10" size="sm"
+            v-on:click="deleteTODO(k)"></UButton>
         </div>
-
       </div>
+
     </div>
   </div>
-</template>
+</div></template>
---
+++ components/ThemeButton.vue
@@ -0,0 +1,28 @@
+<script setup>
+const colorMode = useColorMode()
+
+const isDark = computed({
+  get () {
+    return colorMode.value === 'dark'
+  },
+  set () {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+</script>
+
+<template>
+  <ClientOnly>
+    <UButton
+      :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+      variant="ghost"
+      aria-label="Theme"
+      @click="isDark = !isDark"
+      class="absolute top-4 right-4 dark:text-white text-black dark:bg-primary-400 dark:hover:bg-primary-500 bg-primary-400 hover:bg-primary-300 rounded-full"
+    />
+
+    <template #fallback>
+      <div class="w-8 h-8" />
+    </template>
+  </ClientOnly>
+</template>
