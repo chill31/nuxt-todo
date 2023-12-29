551192362f26027ca862f5f83174f61e5ccef6f7
chill31 <89910163+chill31@users.noreply.github.com>
2023-12-29 22:17:39 +0530

    added edit feature

--- app.vue
+++ app.vue
@@ -12,6 +12,10 @@ const todoFormVisible = ref(false);
 const formTitle = ref('');
 const formValue = ref('');
 
+const editModalVisible = ref(false);
+const editIndex = ref(0);
+const editTitle = ref('');
+const editValue = ref('');
 
 function toggleTODOForm() {
   todoFormVisible.value = !todoFormVisible.value;
@@ -34,13 +38,28 @@ function toggleDone(index) {
   todos.value[index].done = !todos.value[index].done;
   localStorage.setItem('chill31-nuxt-todo-data', JSON.stringify(todos.value));
 }
+
+function openEditModal(index) {
+  editIndex.value = index;
+  editModalVisible.value = true;
+  editTitle.value = todos.value[index].title;
+  editValue.value = todos.value[index].content;
+}
+
+function editTodo() {
+  todos.value[editIndex.value].title = editTitle.value;
+  todos.value[editIndex.value].content = editValue.value;
+  localStorage.setItem('chill31-nuxt-todo-data', JSON.stringify(todos.value));
+  editModalVisible.value = false;
+}
+
 </script>
 
 <template>
   <ThemeButton />
   <div
     class="min-h-screen w-screen overflow-x-hidden flex flex-col gap-12 items-center justify-start dark:bg-slate-900 dark:selection:bg-zinc-600/30 selection:bg-zinc-400/50 bg-slate-200 py-8">
-    <h1 class="text-6xl text-center">TODO</h1>
+    <h1 class="text-6xl text-center font-serif">TODO</h1>
     <p class="text-center">Built with <code
         class="px-2 py-1 border border-white/25 bg-zinc-500/10 rounded-md">Nuxt.js</code> with the help of <code
         class="px-2 py-1 border border-white/25 bg-zinc-600/10 rounded-md">@nuxt/ui</code></p>
@@ -82,21 +101,32 @@ function toggleDone(index) {
 
           <div
             class="flex items-center justify-center gap-2 mt-8 ml-auto px-4 py-2 rounded-md dark:bg-white/10 bg-zinc-400/20">
-          <UButton icon="i-ic-baseline-check" color="transparent"
-            class="dark:text-gray-400 text-gray-700 bg-slate-300 dark:bg-inherit !p-1 focus:outline-current rounded-md hover:bg-white/10"
-            size="sm" v-if="!todo.done" v-on:click="toggleDone(k)"></UButton>
-          <UButton icon="i-bi-x-lg" color="transparent"
-            class="dark:text-gray-400 text-gray-700 bg-slate-300 dark:bg-inherit !p-1 focus:outline-current rounded-md hover:bg-white/10"
-            size="sm" v-if="todo.done" v-on:click="toggleDone(k)"></UButton>
-          <UButton icon="i-mdi-pencil" color="transparent"
-            class="dark:text-primary-400 text-primary-500 dark:bg-inherit bg-slate-300 !p-1 focus:outline-current rounded-md hover:bg-white/10"
-            size="sm"></UButton>
-          <UButton icon="i-heroicons-trash" color="transparent"
-            class="dark:text-red-500 text-red-600 dark:bg-inherit bg-slate-300 !p-1 focus:outline-current rounded-md hover:bg-white/10" size="sm"
-            v-on:click="deleteTODO(k)"></UButton>
+            <UButton icon="i-bi-check-lg" color="transparent"
+              class="dark:text-gray-400 text-gray-700 bg-slate-300 dark:bg-inherit !p-1 focus:outline-current rounded-md hover:bg-white/10"
+              size="sm" v-if="!todo.done" v-on:click="toggleDone(k)"></UButton>
+            <UButton icon="i-bi-x-lg" color="transparent"
+              class="dark:text-gray-400 text-gray-700 bg-slate-300 dark:bg-inherit !p-1 focus:outline-current rounded-md hover:bg-white/10"
+              size="sm" v-if="todo.done" v-on:click="toggleDone(k)"></UButton>
+            <UButton icon="i-mdi-pencil" color="transparent"
+              class="dark:text-primary-400 text-primary-500 dark:bg-inherit bg-slate-300 !p-1 focus:outline-current rounded-md hover:bg-white/10"
+              size="sm" v-on:click="openEditModal(k)"></UButton>
+            <UButton icon="i-heroicons-trash" color="transparent"
+              class="dark:text-red-500 text-red-600 dark:bg-inherit bg-slate-300 !p-1 focus:outline-current rounded-md hover:bg-white/10"
+              size="sm" v-on:click="deleteTODO(k)"></UButton>
+          </div>
         </div>
-      </div>
 
+      </div>
     </div>
+    <UModal v-model="editModalVisible">
+      <div class="h-full w-full p-8 flex flex-col items-start justify-start gap-8">
+        <h2 class="text-3xl">Edit TODO</h2>
+        <div class="flex flex-col items-start justify-start gap-4 w-full">
+          <UInput v-model="editTitle" variant="outline" size="xl" class="w-full" placeholder="enter new TODO title"></UInput>
+          <UInput v-model="editValue" variant="outline" size="xl" class="w-full" placeholder="enter new TODO value"></UInput>
+        </div>
+        <UButton icon="i-bi-check-lg" color="sky" size="lg" class="" v-on:click="editTodo()">Save</UButton>
+      </div>
+    </UModal>
   </div>
-</div></template>
+</template>
